70fa956413070aef417a1d2179dbd3b98b7dbd32
diego gomez <diagomezpa@unal.edu.co>
2019-01-27 21:56:48 -0500

    agregando soap

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
     controllers: {
       sessions: 'sessions'
     }
+
+     wash_out :ws_users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
